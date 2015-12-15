f66c2d86aa34f878beada4c43324288f96441833
aleksandar.arsenovic <salle18@gmail.com>
2015-12-15 12:14:14 +0100

    dodate komponente

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,10 +1,14 @@
 import { Component } from "angular2/angular2";
+import { CsmpMenu } from "../../components/csmp-menu/csmp-menu.controller";
 import { CsmpPanel } from "../../components/csmp-panel/csmp-panel.controller";
+import { CsmpElementList } from "../../components/csmp-element-list/csmp-element-list.controller";
+import { CsmpCanvas } from "../../components/csmp-canvas/csmp-canvas.controller";
+import { CsmpInspector } from "../../components/csmp-inspector/csmp-inspector.controller";
 
 @Component({
 	selector: "page-home",
 	templateUrl: "pages/home/home.template.html",
-	directives: [CsmpPanel]
+	directives: [CsmpMenu, CsmpPanel, CsmpElementList, CsmpCanvas, CsmpInspector]
 })
 export class Home {
 
