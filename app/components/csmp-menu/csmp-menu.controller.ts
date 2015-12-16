916237f8f99e2d5a8e4a8510b81c5e47d20c78ca
aleksandar.arsenovic <salle18@gmail.com>
2015-12-16 15:08:55 +0100

    doradjen meni

--- app/components/csmp-menu/csmp-menu.controller.ts
+++ app/components/csmp-menu/csmp-menu.controller.ts
@@ -1,8 +1,10 @@
 import {Component} from "angular2/angular2";
+import {CsmpUpgradeElement} from "../../directives/csmp-upgrade-element";
 
 @Component({
 	selector: "csmp-menu",
-	templateUrl: "components/csmp-menu/csmp-menu.template.html"
+	templateUrl: "components/csmp-menu/csmp-menu.template.html",
+	directives: [CsmpUpgradeElement]
 })
 export class CsmpMenu {
 
