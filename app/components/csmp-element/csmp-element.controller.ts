63055b7ceeb48d06e89602c45e3d007429c204a1
aleksandar.arsenovic <salle18@gmail.com>
2015-12-16 15:09:49 +0100

    element upgrade direktiva

--- app/components/csmp-element/csmp-element.controller.ts
+++ app/components/csmp-element/csmp-element.controller.ts
@@ -1,10 +1,12 @@
-import {Component, Input} from "angular2/angular2";
+import {Component, Input, ElementRef} from "angular2/angular2";
 import {Element} from "../../csmp/Element";
+import {CsmpUpgradeElement} from "../../directives/csmp-upgrade-element";
 
 
 @Component({
 	selector: "csmp-element",
-	templateUrl: "components/csmp-element/csmp-element.template.html"
+	templateUrl: "components/csmp-element/csmp-element.template.html",
+	directives: [CsmpUpgradeElement]
 })
 export class CsmpElement {
 
