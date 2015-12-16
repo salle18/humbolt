27404af4e2c311380ee8089b8054c3b003f89abe
aleksandar.arsenovic <salle18@gmail.com>
2015-12-16 16:10:53 +0100

    draggable direktiva

--- app/components/csmp-element/csmp-element.controller.ts
+++ app/components/csmp-element/csmp-element.controller.ts
@@ -1,12 +1,13 @@
 import {Component, Input, ElementRef} from "angular2/angular2";
 import {Element} from "../../csmp/Element";
 import {CsmpUpgradeElement} from "../../directives/csmp-upgrade-element";
+import {CsmpDraggable} from "../../directives/csmp-draggable";
 
 
 @Component({
 	selector: "csmp-element",
 	templateUrl: "components/csmp-element/csmp-element.template.html",
-	directives: [CsmpUpgradeElement]
+	directives: [CsmpUpgradeElement, CsmpDraggable]
 })
 export class CsmpElement {
 
