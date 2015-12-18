b62481266c65b860713b1fdea30ac045a190a45c
aleksandar.arsenovic <salle18@gmail.com>
2015-12-18 11:42:08 +0100

    plumb service uvozi jsPlumb

--- app/core/services/PlumbService.ts
+++ app/core/services/PlumbService.ts
@@ -1,14 +1,8 @@
 import "sporritt/jsPlumb";
-import "jqueryui/ui/draggable";
+
 
 declare let jsPlumb:any;
 
 export class PlumbService {
-
-	draggable(element:HTMLElement):void {
-		element.style.position = 'absolute';
-		jsPlumb.draggable(element, {
-			grid: [50, 50]
-		});
-	}
+	
 }
