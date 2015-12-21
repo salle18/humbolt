e1aceffb38946286876f09aa5a0d260ab27cd5b3
aleksandar.arsenovic <salle18@gmail.com>
2015-12-21 16:53:12 +0100

    home drop container

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -4,11 +4,12 @@ import { CsmpPanel } from "../../components/csmp-panel/csmp-panel.controller";
 import { CsmpElementList } from "../../components/csmp-element-list/csmp-element-list.controller";
 import { CsmpCanvas } from "../../components/csmp-canvas/csmp-canvas.controller";
 import { CsmpInspector } from "../../components/csmp-inspector/csmp-inspector.controller";
+import {CsmpDropContainer} from "../../directives/csmp-drop-container";
 
 @Component({
 	selector: "page-home",
 	templateUrl: "pages/home/home.template.html",
-	directives: [CsmpMenu, CsmpPanel, CsmpElementList, CsmpCanvas, CsmpInspector]
+	directives: [CsmpMenu, CsmpPanel, CsmpElementList, CsmpCanvas, CsmpInspector, CsmpDropContainer]
 })
 export class Home {
 
