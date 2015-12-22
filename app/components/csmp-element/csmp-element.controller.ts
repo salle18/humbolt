da6cf105ccf5bbfc9e023f86f85fc00f28ad32c1
aleksandar.arsenovic <salle18@gmail.com>
2015-12-22 15:00:21 +0100

    csmp element novi samo

--- app/components/csmp-element/csmp-element.controller.ts
+++ app/components/csmp-element/csmp-element.controller.ts
@@ -1,15 +1,10 @@
 import {Component, Input} from "angular2/angular2";
-import {Element} from "../../csmp/Element";
-import {CsmpUpgradeElement} from "../../directives/csmp-upgrade-element";
-import {CsmpCloneElement} from "../../directives/csmp-clone-element";
 
 @Component({
 	selector: "csmp-element",
-	templateUrl: "components/csmp-element/csmp-element.template.html",
-	directives: [CsmpUpgradeElement, CsmpCloneElement]
+	templateUrl: "components/csmp-element/csmp-element.template.html"
 })
 export class CsmpElement {
-
 	@Input() element:Element;
 
 }
