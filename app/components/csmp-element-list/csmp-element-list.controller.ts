cde7a364b952c4ab4cbe9b7e2f36c3836590d874
aleksandar.arsenovic <salle18@gmail.com>
2015-12-22 14:48:55 +0100

    csmp-element-list-item

--- app/components/csmp-element-list/csmp-element-list.controller.ts
+++ app/components/csmp-element-list/csmp-element-list.controller.ts
@@ -1,12 +1,12 @@
 import {Component, NgFor} from "angular2/angular2";
 import {Element} from "../../csmp/Element";
 import * as ElementDefinitions from "../../csmp/ElementDefinitions";
-import {CsmpElement} from "../csmp-element/csmp-element.controller";
+import {CsmpElementListItem} from "../csmp-element-list-item/csmp-element-list-item.controller";
 
 @Component({
 	selector: "csmp-element-list",
 	templateUrl: "components/csmp-element-list/csmp-element-list.template.html",
-	directives: [NgFor, CsmpElement]
+	directives: [NgFor, CsmpElementListItem]
 })
 export class CsmpElementList {
 
