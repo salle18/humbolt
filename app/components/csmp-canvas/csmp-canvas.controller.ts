b6a0db4765206c2b2ee53c2c0a8ceed08c3fe41e
aleksandar.arsenovic <salle18@gmail.com>
2015-12-22 13:47:55 +0100

    logika iz drop container direktive prebacena u kontroler

--- app/components/csmp-canvas/csmp-canvas.controller.ts
+++ app/components/csmp-canvas/csmp-canvas.controller.ts
@@ -1,9 +1,27 @@
-import {Component} from "angular2/angular2";
+import {Component, ElementRef} from "angular2/angular2";
+import "jquery-ui/ui/droppable";
 
 @Component({
 	selector: "csmp-canvas",
 	templateUrl: "components/csmp-canvas/csmp-canvas.template.html"
 })
 export class CsmpCanvas {
+
+	constructor(elementRef:ElementRef) {
+		let nativeElement = elementRef.nativeElement;
+
+		jQuery(nativeElement).droppable({
+			accept: ":not(.csmp-canvas-element)",
+			drop: (event, ui) => {
+				let node = ui.helper.clone();
+				node.addClass("csmp-canvas-element");
+				nativeElement.appendChild(node[0]);
+				let className = node.attr("className");
+				node.draggable({
+					containment: "parent"
+				});
+			}
+		});
+	}
 
 }
