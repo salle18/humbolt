d2547866873e84e908943b795947d03f148cdc78
aleksandar.arsenovic <salle18@gmail.com>
2015-12-22 16:19:59 +0100

    canvas controller

--- app/components/csmp-canvas/csmp-canvas.controller.ts
+++ app/components/csmp-canvas/csmp-canvas.controller.ts
@@ -1,5 +1,10 @@
 import {Component, ElementRef} from "angular2/angular2";
+import {KeyEvent} from "../../core/commons/KeyEvent";
+import {Element, IPosition} from "../../csmp/Element";
+import * as ElementDefinitions from "../../csmp/ElementDefinitions";
+import {CsmpElement} from "../../components/csmp-element/csmp-element.controller";
 import "jquery-ui/ui/droppable";
+
 
 @Component({
 	selector: "csmp-canvas",
@@ -7,19 +12,44 @@ import "jquery-ui/ui/droppable";
 })
 export class CsmpCanvas {
 
-	constructor(elementRef:ElementRef) {
-		let nativeElement = elementRef.nativeElement;
+	private nativeElement:HTMLElement;
 
-		jQuery(nativeElement).droppable({
+	constructor(elementRef:ElementRef) {
+
+		this.nativeElement = elementRef.nativeElement;
+
+		jQuery(this.nativeElement).droppable({
 			accept: ":not(.csmp-canvas-element)",
 			drop: (event, ui) => {
-				let node = ui.helper.clone();
-				node.addClass("csmp-canvas-element");
-				nativeElement.appendChild(node[0]);
-				let className = node.attr("className");
-				node.draggable({
-					containment: "parent"
-				});
+				//todo uzimanje naziva klase iz elementa
+				let className = ui.helper.find("div").attr("classname");
+				let element:Element = new ElementDefinitions[className];
+				element.position = ui.helper.position();
+				this.addCanvasElement(element);
+			}
+		});
+	}
+
+	addCanvasElement(element:Element):void {
+		//todo kreiranje novog elementa iz direktive csmp-element a ne na silu ovako
+		let node = jQuery("<div>").addClass("csmp-canvas-element mdl-button mdl-button--primary mdl-button--raised");
+		node.text(element.getSign());
+		node.css('position', 'absolute').css(element.position);
+		jQuery(this.nativeElement).append(node);
+
+		node.draggable({
+			containment: "parent"
+		});
+
+		node.on("mousedown", () => {
+			//set active element
+
+		});
+
+		node.on("keydown", (e) => {
+			if (e.keyCode === KeyEvent.DOM_VK_DELETE) {
+				//remove element
+
 			}
 		});
 	}
