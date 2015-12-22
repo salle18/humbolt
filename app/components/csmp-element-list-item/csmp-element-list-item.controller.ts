123b1316235b71a613410bee18568e964dd23ac9
aleksandar.arsenovic <salle18@gmail.com>
2015-12-22 14:47:18 +0100

    csmp element list item

--- app/components/csmp-element-list-item/csmp-element-list-item.controller.ts
+++ app/components/csmp-element-list-item/csmp-element-list-item.controller.ts
@@ -1,12 +1,12 @@
 import {Component, Input} from "../../../node_modules/angular2/angular2.d";
 import {Element} from "../../csmp/Element";
-import {CsmpUpgradeElement} from "../../directives/csmp-upgrade-element";
+import {CsmpElement} from "../../components/csmp-element/csmp-element.controller";
 import {CsmpCloneElement} from "../../directives/csmp-clone-element";
 
 @Component({
 	selector: "csmp-element-list-item",
 	templateUrl: "components/csmp-element-list-item/csmp-element-list-item.template.html",
-	directives: [CsmpUpgradeElement, CsmpCloneElement]
+	directives: [CsmpElement, CsmpCloneElement]
 })
 export class CsmpElementListItem {
 
