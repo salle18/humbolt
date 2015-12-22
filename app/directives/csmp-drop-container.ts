897c002967b4a5c7dce47a5d306e01981103131e
aleksandar.arsenovic <salle18@gmail.com>
2015-12-22 12:56:24 +0100

    sprecavamo umnozavanje vec dropovanih elemenata

--- app/directives/csmp-drop-container.ts
+++ app/directives/csmp-drop-container.ts
@@ -12,8 +12,10 @@ export class CsmpDropContainer {
 		let nativeElement = elementRef.nativeElement;
 
 		jQuery(nativeElement).droppable({
+			accept: ":not(.drop-container-dropped)",
 			drop: (event, ui) => {
 				let node = ui.helper.clone();
+				node.addClass("drop-container-dropped");
 				nativeElement.appendChild(node[0]);
 				let className = node.attr("className");
 				node.draggable({
