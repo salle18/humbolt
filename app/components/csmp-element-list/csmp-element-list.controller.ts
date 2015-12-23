455541359a01d7c08acc123140f4d5a77db4ed6a
aleksandar.arsenovic <salle18@gmail.com>
2015-12-23 10:58:45 +0100

    csmp clone element

--- app/components/csmp-element-list/csmp-element-list.controller.ts
+++ app/components/csmp-element-list/csmp-element-list.controller.ts
@@ -1,12 +1,13 @@
 import {Component, NgFor} from "angular2/angular2";
 import {Element} from "../../csmp/Element";
 import * as ElementDefinitions from "../../csmp/ElementDefinitions";
+import {CsmpCloneElement} from "../../directives/csmp-clone-element";
 
 
 @Component({
 	selector: "csmp-element-list",
 	templateUrl: "components/csmp-element-list/csmp-element-list.template.html",
-	directives: [NgFor]
+	directives: [NgFor, CsmpCloneElement]
 })
 export class CsmpElementList {
 
