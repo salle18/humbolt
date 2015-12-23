60788d27cb8a63bdbf0a038df02198409aabd202
aleksandar.arsenovic <salle18@gmail.com>
2015-12-23 10:25:08 +0100

    draggable i instanca

--- app/core/services/PlumbService.ts
+++ app/core/services/PlumbService.ts
@@ -4,4 +4,18 @@ import "sporritt/jsPlumb";
 declare let jsPlumb:any;
 
 export class PlumbService {
+
+	private instance:JsPlumbInstance;
+
+	constructor() {
+		jsPlumb.ready(() => {
+			this.instance = jsPlumb.getInstance();
+		});
+	}
+	
+	draggable(id:string) {
+		this.instance.draggable(id, {
+			containment: "parent"
+		});
+	}
 }
