3ff7dddf1a2b2544b9378c9701cc5f08ca00b0b9
salle <S@lle#18>
2015-12-23 20:07:45 +0100

    dodat template inspectora

--- app/components/csmp-inspector/csmp-inspector.controller.ts
+++ app/components/csmp-inspector/csmp-inspector.controller.ts
@@ -1,9 +1,22 @@
 import {Component} from "angular2/angular2";
+import {Element} from "../../csmp/Element";
+import {SimulationService} from "../../core/services/SimulationService";
 
 @Component({
 	selector: "csmp-inspector",
 	templateUrl: "components/csmp-inspector/csmp-inspector.template.html"
 })
 export class CsmpInspector {
+
+	private simulationService: SimulationService = null;
+	public elements: Element[] = [];
+	public params: number[] = [];
+	public stringParams: string[] = [];
+	public inputs: Element[] = [];
+	public outputs: Element[] = [];
+	
+	constructor(simulationService: SimulationService) {
+		this.elements = simulationService.getElements();
+	}
 
 }
