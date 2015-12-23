01a9e8e9d27c2b6cc0111a73207d45b256417317
aleksandar.arsenovic <salle18@gmail.com>
2015-12-23 10:29:37 +0100

    izbacen csmp element list item nema potrebe sve stavljeno u template csmp element list

--- app/components/csmp-element-list/csmp-element-list.controller.ts
+++ app/components/csmp-element-list/csmp-element-list.controller.ts
@@ -1,12 +1,12 @@
 import {Component, NgFor} from "angular2/angular2";
 import {Element} from "../../csmp/Element";
 import * as ElementDefinitions from "../../csmp/ElementDefinitions";
-import {CsmpElementListItem} from "../csmp-element-list-item/csmp-element-list-item.controller";
+
 
 @Component({
 	selector: "csmp-element-list",
 	templateUrl: "components/csmp-element-list/csmp-element-list.template.html",
-	directives: [NgFor, CsmpElementListItem]
+	directives: [NgFor]
 })
 export class CsmpElementList {
 
