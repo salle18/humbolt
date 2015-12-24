a7dcbf51ed633d92783c6a331e3db56f92f6583c
aleksandar.arsenovic <salle18@gmail.com>
2015-12-24 13:21:00 +0100

    csmp block list

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from "angular2/angular2";
 import { CsmpMenu } from "../../components/csmp-menu/csmp-menu.controller";
 import { CsmpPanel } from "../../components/csmp-panel/csmp-panel.controller";
-import { CsmpElementList } from "../../components/csmp-element-list/csmp-element-list.controller";
+import { CsmpBlockList } from "../../components/csmp-block-list/csmp-block-list.controller";
 import { CsmpCanvas } from "../../components/csmp-canvas/csmp-canvas.controller";
 import { CsmpInspector } from "../../components/csmp-inspector/csmp-inspector.controller";
 import {CsmpPannable} from "../../directives/csmp-pannable";
@@ -9,7 +9,7 @@ import {CsmpPannable} from "../../directives/csmp-pannable";
 @Component({
 	selector: "page-home",
 	templateUrl: "pages/home/home.template.html",
-	directives: [CsmpMenu, CsmpPanel, CsmpElementList, CsmpCanvas, CsmpPannable, CsmpInspector]
+	directives: [CsmpMenu, CsmpPanel, CsmpBlockList, CsmpCanvas, CsmpPannable, CsmpInspector]
 })
 export class Home {
 
