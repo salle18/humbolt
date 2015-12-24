ea8f873f0a141e78d9ed7fefef1cb4fc625c886d
aleksandar.arsenovic <salle18@gmail.com>
2015-12-24 11:22:23 +0100

    rotate prosireno levo i desno

--- app/core/services/PlumbServiceUtilities.ts
+++ app/core/services/PlumbServiceUtilities.ts
@@ -11,22 +11,34 @@ export class PlumbServiceUtilities {
 		this.plumbService = plumbService;
 	}
 
-	rotate(element:Element) {
-		let transforms = {
-			Right: "Bottom",
-			Bottom: "Left",
-			Left: "Top",
-			Top: "Right",
-			TopRight: "BottomRight",
-			BottomRight: "BottomLeft",
-			BottomLeft: "TopLeft",
-			TopLeft: "TopRight"
+	rotate(element:Element, direction:string) {
+		let rotations = {
+			Right: {
+				Right: "Bottom",
+				Bottom: "Left",
+				Left: "Top",
+				Top: "Right",
+				TopRight: "BottomRight",
+				BottomRight: "BottomLeft",
+				BottomLeft: "TopLeft",
+				TopLeft: "TopRight"
+			},
+			Left: {
+				Right: "Top",
+				Bottom: "Right",
+				Left: "Bottom",
+				Top: "Left",
+				TopRight: "TopLeft",
+				BottomRight: "TopRight",
+				BottomLeft: "BottomRight",
+				TopLeft: "BottomLeft"
+			}
 		};
 
 		let endpoints = this.plumbService.getInstance().selectEndpoints({element: element.key});
 		endpoints.each(function (endpoint:any) {
 			let type = endpoint.anchor.type;
-			endpoint.setAnchor(transforms[type], true);
+			endpoint.setAnchor(rotations[direction][type], true);
 		});
 		this.plumbService.getInstance().repaint(element.key);
 	}
