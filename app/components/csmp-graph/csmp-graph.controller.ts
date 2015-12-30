80d1e180ad71d36332860fd1b6e09a316019b061
aleksandar.arsenovic <salle18@gmail.com>
2015-12-30 16:11:06 +0100

    csmp graph skeleton

--- app/components/csmp-graph/csmp-graph.controller.ts
+++ app/components/csmp-graph/csmp-graph.controller.ts
@@ -1,3 +1,9 @@
+import {Component} from "angular2/angular2";
+
+@Component({
+	selector: "csmp-graph",
+	templateUrl: "components/csmp-graph/csmp-graph.template.html"
+})
 export class CsmpGraph {
 
 }
