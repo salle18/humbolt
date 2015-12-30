bf1e7fe5f68f04086acf5b6f8c41f92d7e8b730d
aleksandar.arsenovic <salle18@gmail.com>
2015-12-30 15:54:00 +0100

    skeleton za csmp graph

--- app/pages/graph/graph.ts
+++ app/pages/graph/graph.ts
@@ -1,3 +1,14 @@
+import { Component } from "angular2/angular2";
+import {CsmpGraph} from "../../components/csmp-graph/csmp-graph.controller";
+
+@Component({
+	selector: "page-graph",
+	templateUrl: "pages/graph/graph.template.html",
+	directives: [CsmpGraph]
+})
 export class Graph {
 
+	constructor() {
+		console.log("Graph component loaded");
+	}
 }
