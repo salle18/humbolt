736ac0d2928f83797f2918eec4076f230a6fb736
aleksandar.arsenovic <salle18@gmail.com>
2016-01-19 11:27:09 +0100

    iz simulacije izbacena sva logika nepotrebna za klijenta

--- app/csmp/Simulation.ts
+++ app/csmp/Simulation.ts
@@ -1,50 +1,22 @@
-import {Block, IPosition} from "./Block";
-import * as Exception from "./helpers/Exception";
-import {IntegrationMethod} from "./IntegrationMethod";
-import * as IntegrationMethodDefinitions from "./IntegrationMethodDefinitions";
+import {Block, IJSONBlock, IPosition} from "./Block";
 import {Dictionary} from "./helpers/Dictionary";
-import * as BlockDefinitions from "./BlockDefinitions";
 
-/**
- * JSON format u kome se čuvaju blocki simulacije i koji se šalje serveru.
- */
-interface IJSONBlock {
-	className: string;
-	position: IPosition;
-	params: number[];
-	stringParams: string[];
-	inputs: number[];
-}
 
 export class Simulation {
 	/**
 	 * Niz elemenata u simulaciji.
 	 */
 	private blocks:Dictionary<Block>;
-	/**
-	 * Trenutno vreme izvršavanja simulacije.
-	 */
-	private timer:number = 0;
+
 	/**
 	 * Trajanje simulacije.
 	 */
 	private duration:number = 0;
+
 	/**
 	 * Interval integracije.
 	 */
 	private integrationInterval:number = 0;
-	/**
-	 * Niz sortiranih elemenata simulacije.
-	 */
-	private sorted:Block[] = [];
-	/**
-	 * Svi rezultati simulacije.
-	 */
-	private results:number[][] = [];
-	/**
-	 * Niz integratora.
-	 */
-	public integrators:BlockDefinitions.Integrator[] = [];
 
 	constructor() {
 		this.blocks = new Dictionary<Block>();
@@ -58,9 +30,6 @@ export class Simulation {
 	 */
 	addBlock(block:Block):void {
 		block.setSimulation(this);
-		if (block instanceof BlockDefinitions.Integrator) {
-			this.integrators.push(block);
-		}
 		let key = this.blocks.add(block);
 		block.key = key;
 	}
@@ -76,10 +45,6 @@ export class Simulation {
 		let blocks = this.blocks.getValues();
 		for (let i = 0; i < blocks.length; i++) {
 			blocks[i].removeReference(block);
-		}
-		if (block instanceof BlockDefinitions.Integrator) {
-			let index = this.integrators.indexOf(block);
-			this.integrators.splice(index, 1);
 		}
 		this.blocks.remove(key);
 	}
@@ -112,128 +77,6 @@ export class Simulation {
 	 */
 	getKeys():string[] {
 		return this.blocks.getKeys();
-	}
-
-	/**
-	 * Pokretanje simulacije.
-	 *
-	 * @param method Naziv klase metode simulacije.
-	 * @param integrationInterval Interval integracije.
-	 * @param duration Trajanje simulacije.
-	 */
-	run(method:string, integrationInterval:number, duration:number):void {
-		/**
-		 * Restujemo niz rezultata. Postavljamo interval i trajanje i resetujemo vreme.
-		 */
-		this.results.length = 0;
-		this.duration = duration;
-		this.integrationInterval = integrationInterval;
-		this.timer = 0;
-		/**
-		 * Pravimo instancu metode integracije i prosleđujemo trenutno instancu simulacije kao parametar.
-		 */
-		let integrationMethod:IntegrationMethod = new IntegrationMethodDefinitions[method](this);
-		try {
-			/**
-			 * Pre pokretanja integracije vršimo sve provere.
-			 */
-			this.preRunCheck();
-			/**
-			 * Resetujemo rezultate elemenata i inicijalizujemo ih.
-			 */
-			this.initBlocks();
-			/**
-			 * Sortiramo blocke po njihovim ulazima.
-			 */
-			this.sortBlocks();
-			/**
-			 * Pokrećemo metodu integracije.
-			 */
-			integrationMethod.run();
-		} catch (e) {
-			/**
-			 * Ukoliko je nastala greška tipa QuitSimulation nije došlo do nepredviđenog izuzetka već je simulacija završena dolaskom do Quit bloka.
-			 */
-			if (!(e instanceof Exception.QuitSimulation)) {
-				throw e;
-			}
-		}
-	}
-
-	/**
-	 * Izračunava rezulatate za sve blocke.
-	 */
-	setResults():void {
-		let blocks = this.sorted;
-		for (let i = 0; i < blocks.length; i++) {
-			blocks[i].calculateResult();
-		}
-		/**
-		 * Za sve integratore ponovo pozivamo calculateResult kako bi se izračunao novi rezultat.
-		 */
-		for (let i = 0; i < this.integrators.length; i++) {
-			this.integrators[i].calculateResult();
-		}
-	}
-
-	/**
-	 * @return Trenutni rezultati svih elemenata.
-	 */
-	getResults():number[] {
-		let results:number[] = [this.timer];
-		let blocks = this.blocks.getValues();
-		for (let i = 0; i < blocks.length; i++) {
-			results.push(blocks[i].result);
-		}
-		return results;
-	}
-
-	/**
-	 * Čuva trenutne rezultate.
-	 */
-	saveResults():void {
-		this.results.push(this.getResults());
-	}
-
-	/**
-	 * @return Svi rezultati simulacije.
-	 */
-	getSimulationResults():number[][] {
-		return this.results;
-	}
-
-	/**
-	 * Provera parametara simulacije pre pokretanja.
-	 */
-	preRunCheck():void {
-		if (!(this.duration > 0)) {
-			throw new Exception.Simulation("Dužina simulacije mora biti veća od nule.");
-		}
-		if (!(this.integrationInterval > 0)) {
-			throw new Exception.Simulation("Interval integracije mora biti veća od nule.");
-		}
-		if (this.duration < this.integrationInterval) {
-			throw new Exception.Simulation("Dužina simulacije mora biti veća od intervala integracije.");
-		}
-		if (this.blocks.getValues().length === 0) {
-			throw new Exception.Simulation("Simulacija mora da ima najmanje jedan block.");
-		}
-	}
-
-	/**
-	 * @return Trenutno vreme izvršavanja simulacije.
-	 */
-	getTimer():number {
-		return this.timer;
-	}
-
-	/**
-	 * Povećava timer za zadati deo intervala integracije.
-	 *
-	 * @param times Koliko delova intervala integracije.
-	 */
-	incrementInterval(times:number):void {
-		this.timer += times * this.integrationInterval;
 	}
 
 	/**
@@ -278,60 +121,6 @@ export class Simulation {
 	}
 
 	/**
-	 * Inicijalizacija elemenata. Rezultati se postavljaju na 0 a zatim se poziva init svakog blocka.
-	 */
-	initBlocks():void {
-		let blocks = this.blocks.getValues();
-		for (let i = 0; i < blocks.length; i++) {
-			blocks[i].result = 0;
-			blocks[i].init();
-		}
-	}
-
-	/**
-	 * Sortira blocke iz niza elemenata.
-	 * Konstante, integratori i unitDelay se odmah sortiraju jer je rezulat izračunavanja poznat iz prethodnog intervala(previousValue). Ostali blocki moraju da imaju sve sortirane ulaze da bi se sortirali.
-	 */
-	sortBlocks() {
-		this.sorted.length = 0;
-		let numberOfSorted = 0;
-		let blocks = this.blocks.getValues();
-		for (let i = 0; i < blocks.length; i++) {
-			blocks[i].sorted = false;
-			this.sorted.push(blocks[i]);
-		}
-		let finished = false;
-		while (numberOfSorted < this.sorted.length && !finished) {
-			finished = true;
-			for (let i = numberOfSorted; i < this.sorted.length; i++) {
-				let block = this.sorted[i];
-				if (block instanceof BlockDefinitions.Integrator || block instanceof BlockDefinitions.Constant || block.hasSortedInputs()) {
-					let temp = this.sorted[numberOfSorted];
-					this.sorted[numberOfSorted] = block;
-					block.sorted = true;
-					this.sorted[i] = temp;
-					numberOfSorted++;
-					finished = false;
-				}
-			}
-		}
-	}
-
-	/**
-	 * @return Da li u simulaciji ima elemenata koji se izračunavaju na serverskoj strani.
-	 */
-	hasRemoteBlocks():boolean {
-		let hasRemote = false;
-		let blocks = this.blocks.getValues();
-		for (let i = 0; i < blocks.length; i++) {
-			if (blocks[i].remote) {
-				hasRemote = true;
-			}
-		}
-		return hasRemote;
-	}
-
-	/**
 	 * Čuvamo nazive klasa elemenata, poziciju, parametre i ulaze kao indekse niza.
 	 * Nema potrebe da čuvamo izlaze jer ćemo ih rekonstruisati iz ulaza.
 	 *
@@ -366,7 +155,7 @@ export class Simulation {
 		for (let i = 0; i < JSONBlocks.length; i++) {
 			let JSONBlock = JSONBlocks[i];
 			let className = JSONBlock.className;
-			let block = new BlockDefinitions[className];
+			let block = new Block();
 			block.params = JSONBlock.params;
 			block.stringParams = JSONBlock.stringParams;
 			block.position = JSONBlock.position;
@@ -399,10 +188,6 @@ export class Simulation {
 	 */
 	reset():void {
 		this.blocks.reset();
-		this.sorted.length = 0;
-		this.results.length = 0;
-		this.integrators.length = 0;
-		this.timer = 0;
 		this.duration = 0;
 		this.integrationInterval = 0;
 	}
