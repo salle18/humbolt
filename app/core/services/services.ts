a698fdab9a7b0c762309e6dfc3bb4e8082c44682
aleksandar.arsenovic <salle18@gmail.com>
2016-01-19 12:40:21 +0100

    server service dodat

--- app/core/services/services.ts
+++ app/core/services/services.ts
@@ -2,7 +2,8 @@ import {AppService} from "./AppService";
 import {SimulationService} from "./SimulationService";
 import {PlumbService} from "./PlumbService";
 import {PlumbServiceUtilities} from "./PlumbServiceUtilities";
+import {ServerService} from "./ServerService";
 
 export let appServicesInjectables:Array<any> = [
-	AppService, SimulationService, PlumbService, PlumbServiceUtilities
+	AppService, SimulationService, PlumbService, PlumbServiceUtilities, ServerService
 ];
