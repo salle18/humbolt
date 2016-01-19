9a656b277149c58d6fae3034e6b9dc5116b7e80b
aleksandar.arsenovic <salle18@gmail.com>
2016-01-19 15:31:50 +0100

    dodat getBlocks Observable, nista mi nije jasno kako ovo radi

--- app/core/services/ServerService.ts
+++ app/core/services/ServerService.ts
@@ -1,10 +1,18 @@
-import {Injectable} from "angular2/angular2";
-import {Block} from "../../csmp/Block";
+import {Injectable, Observable} from "angular2/angular2";
+import {Http, Response} from "angular2/http";
 
 @Injectable()
 export class ServerService {
 
-	getBlocks():Block[] {
-		return [];
+	private http:Http;
+	private url = "http://localhost:9000/csmp";
+
+	constructor(http:Http) {
+		this.http = http;
+	}
+
+	getBlocks():Observable<Response> {
+		return this.http.get("http://localhost:9000/csmp/blocks")
+			.map(res => (res as Response).json());
 	}
 }
