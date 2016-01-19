05fcba50894607e3f05c5fe1a93e9cf22f212bd7
aleksandar.arsenovic <salle18@gmail.com>
2016-01-19 15:34:09 +0100

    dodat getBlocks Observable, nista mi nije jasno kako ovo radi, posebno mapiranje ovo treba bas istraziti

--- app/components/csmp-block-list/csmp-block-list.controller.ts
+++ app/components/csmp-block-list/csmp-block-list.controller.ts
@@ -1,5 +1,5 @@
 import {Component} from "angular2/angular2";
-import {Block} from "../../csmp/Block";
+import {Block, IMetaJSONBlock} from "../../csmp/Block";
 import {CsmpCloneBlock} from "../../directives/csmp-clone-block";
 import {ServerService} from "../../core/services/ServerService";
 
@@ -10,11 +10,25 @@ import {ServerService} from "../../core/services/ServerService";
 })
 export class CsmpBlockList {
 
+	private serverService:ServerService;
+
 	public blocks:Block[] = [];
 
 	constructor(serverService:ServerService) {
+		this.serverService = serverService;
+		this.getBlocks();
+	}
 
-		this.blocks = serverService.getBlocks();
-
+	getBlocks() {
+		this.serverService.getBlocks().map(res => {
+			let blocks = [];
+			for (let i = 0; i < (res as IMetaJSONBlock[]).length; i++) {
+				blocks[i] = new Block();
+				blocks[i].loadMetaJSON(res[i]);
+			}
+			return blocks;
+		}).subscribe(
+			blocks => this.blocks = blocks
+		);
 	}
 }
