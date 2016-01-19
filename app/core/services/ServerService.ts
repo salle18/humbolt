357e2a9a617d9a4ec5232e15987b2a0d989ed2c0
aleksandar.arsenovic <salle18@gmail.com>
2016-01-19 12:38:05 +0100

    Server service skeleton

--- app/core/services/ServerService.ts
+++ app/core/services/ServerService.ts
@@ -1,9 +1,10 @@
 import {Injectable} from "angular2/angular2";
+import {Block} from "../../csmp/Block";
 
 @Injectable()
 export class ServerService {
 
-	getBlocks() {
-
+	getBlocks():Block[] {
+		return [];
 	}
 }
