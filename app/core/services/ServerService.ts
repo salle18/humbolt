bd59e8c335b6f6f846b4adb402620ce30e9744aa
aleksandar.arsenovic <salle18@gmail.com>
2016-01-20 12:14:38 +0100

    post simulate

--- app/core/services/ServerService.ts
+++ app/core/services/ServerService.ts
@@ -1,5 +1,6 @@
 import {Injectable, Observable} from "angular2/angular2";
 import {Http, Response} from "angular2/http";
+import {IJSONSimulation} from "../../csmp/Simulation";
 
 @Injectable()
 export class ServerService {
@@ -13,6 +14,11 @@ export class ServerService {
 
 	getMetaBlocks():Observable<Response> {
 		return this.http.get("http://localhost:9000/csmp/blocks")
+			.map(res => (res as Response).json());
+	}
+
+	postSimulate(data:string):Observable<Response> {
+		return this.http.post("http://localhost:9000/csmp/simulate", data)
 			.map(res => (res as Response).json());
 	}
 }
