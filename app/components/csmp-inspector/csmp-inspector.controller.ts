3055833d35e3854ebb2326006dca0b487dddc3f0
aleksandar.arsenovic <salle18@gmail.com>
2016-01-21 11:04:37 +0100

    param i string params

--- app/components/csmp-inspector/csmp-inspector.controller.ts
+++ app/components/csmp-inspector/csmp-inspector.controller.ts
@@ -1,15 +1,7 @@
 import {Component, DoCheck} from "angular2/angular2";
-import {Block} from "../../csmp/Block";
+import {Block, IParam} from "../../csmp/Block";
 import {SimulationService} from "../../core/services/SimulationService";
 import {AppService} from "../../core/services/AppService";
-
-interface IValueNumber {
-	value: number;
-}
-
-interface IValueString {
-	value: string;
-}
 
 @Component({
 	selector: "csmp-inspector",
@@ -21,8 +13,8 @@ export class CsmpInspector implements DoCheck {
 	private simulationService:SimulationService = null;
 	public blocks:Block[] = [];
 	public activeBlock:Block = null;
-	public params:IValueNumber[] = [];
-	public stringParams:IValueString[] = [];
+	public params:IParam<number>[] = [];
+	public stringParams:IParam<string>[] = [];
 
 	constructor(appService:AppService, simulationService:SimulationService) {
 		this.appService = appService;
@@ -34,33 +26,8 @@ export class CsmpInspector implements DoCheck {
 		this.appService.setActiveBlock(block);
 	}
 
-	setActiveBlockParam(i, value) {
-		this.params[i].value = value;
-		this.activeBlock.params[i] = value;
-	}
-
-	setActiveBlockStringParam(i, value) {
-		this.stringParams[i].value = value;
-		this.activeBlock.stringParams[i] = value;
-	}
-
-	doCheck():void {
+	doCheck() {
 		this.activeBlock = this.appService.activeBlock;
-		if (this.activeBlock) {
-			this.params = this.activeBlock.params.map(item => {
-				return {
-					value: item
-				};
-			});
-			this.stringParams = this.activeBlock.stringParams.map(item => {
-				return {
-					value: item
-				};
-			});
-		} else {
-			this.params = [];
-			this.stringParams = [];
-		}
 	}
 
 }
