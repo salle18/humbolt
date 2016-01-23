14f9591a8bb030a8a948638273d1d32fbb46d0aa
salle18 <salle18@gmail.com>
2016-01-23 22:34:51 +0100

    server service uniforman

--- app/core/services/ServerService.ts
+++ app/core/services/ServerService.ts
@@ -1,6 +1,6 @@
 import "rxjs/add/operator/map";
 import {Injectable} from "angular2/core";
-import {Http, Response, Headers} from "angular2/http";
+import {Http, Response, Headers, RequestOptionsArgs} from "angular2/http";
 import {IJSONSimulation, IMetaJSONMethod} from "../../csmp/Simulation";
 import {Observable} from "rxjs/Observable";
 import {IMetaJSONBlock} from "../../csmp/Block";
@@ -9,29 +9,34 @@ import {IMetaJSONBlock} from "../../csmp/Block";
 export class ServerService {
 
 	private http:Http;
-	private url = "http://localhost:9000/csmp";
+	private url = "http://localhost:9000";
+	private api = "http://localhost:9000/api";
+	private headers:Headers;
 
 	constructor(http:Http) {
 		this.http = http;
+		this.headers = new Headers();
+		this.headers.set("Content-Type", "application/json");
+	}
+
+	getRequestOptions():RequestOptionsArgs {
+		return {
+			headers: this.headers
+		}
 	}
 
 	getMetaBlocks():Observable<IMetaJSONBlock[]> {
-		return this.http.get("http://localhost:9000/api/csmp/blocks")
+		return this.http.get(this.api + "/csmp/blocks", this.getRequestOptions())
 			.map(res => res.json());
 	}
 
 	getIntegrationMethods():Observable<IMetaJSONMethod[]> {
-		return this.http.get("http://localhost:9000/api/csmp/integrationmethods")
+		return this.http.get(this.api + "/csmp/integrationmethods", this.getRequestOptions())
 			.map(res => res.json());
 	}
 
-	postSimulate(JSONSimulation:IJSONSimulation):Observable<any> {
-		let headers = new Headers({
-			"Content-Type": "application/json"
-		});
-		return this.http.post("http://localhost:9000/api/csmp/simulate", JSON.stringify(JSONSimulation), {
-				headers: headers
-			})
+	postSimulation(JSONSimulation:IJSONSimulation):Observable<any> {
+		return this.http.post(this.api + "/csmp/simulate", JSON.stringify(JSONSimulation), this.getRequestOptions())
 			.map(res => res.json());
 	}
 }
