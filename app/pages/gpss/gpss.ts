deb81ba13fd520708f41073e3a0de6bd33b09698
salle18 <salle18@gmail.com>
2016-01-24 21:52:39 +0100

    gpss skeleton

--- app/pages/gpss/gpss.ts
+++ app/pages/gpss/gpss.ts
@@ -1,11 +1,12 @@
 "use strict";
 
 import {Component} from "angular2/core";
+import {RouterLink} from "angular2/router";
 
 @Component({
 	selector: "page-gpss",
 	templateUrl: "pages/gpss/gpss.template.html",
-	directives: []
+	directives: [RouterLink]
 })
 export class Gpss {
 
