b96a550fe217fa9b835f2232f64c7e8f3acff938
salle18 <salle18@gmail.com>
2016-01-24 21:45:21 +0100

    hub template i logic

--- app/pages/hub/hub.ts
+++ app/pages/hub/hub.ts
@@ -1,15 +1,28 @@
 "use strict";
 
 import {Component} from "angular2/core";
+import {RouterLink} from "angular2/router";
+import {CsmpUpgradeElement} from "../../directives/csmp-upgrade-element";
+import {AppService} from "../../core/services/AppService";
 
 @Component({
 	selector: "page-hub",
 	templateUrl: "pages/hub/hub.template.html",
-	directives: []
+	directives: [CsmpUpgradeElement, RouterLink]
 })
 export class Hub {
-
-	constructor() {
-		console.log("Hub component loaded");
+	
+	private appService: AppService;
+	
+	public user = {
+		name: "Korisnik"	
+	};
+	
+	constructor(appService: AppService) {
+		this.appService = appService;
+	}
+	
+	logout(): void {
+		this.appService.logout();
 	}
 }
