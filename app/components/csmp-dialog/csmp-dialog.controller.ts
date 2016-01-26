d7b465b14e79e1d6469e6144c10dfcfb0d6c2468
aleksandar.arsenovic <salle18@gmail.com>
2016-01-26 13:03:38 +0100

    simulations

--- app/components/csmp-dialog/csmp-dialog.controller.ts
+++ app/components/csmp-dialog/csmp-dialog.controller.ts
@@ -1,12 +1,17 @@
 import {Component} from "angular2/core";
 import {IJSONSimulation} from "../../csmp/Simulation";
+import {AppService} from "../../core/services/AppService";
 
 @Component({
 	selector: "csmp-dialog",
 	templateUrl: "components/csmp-dialog/csmp-dialog.template.html"
 })
 export class CsmpDialog {
-	
-	private simulations:IJSONSimulation[] = [];
+
+	private simulations:IJSONSimulation[];
+
+	constructor(private appService:AppService) {
+		this.simulations = this.appService.simulations;
+	}
 
 }
