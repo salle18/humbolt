7ea5c3516ef88571260beb4076354d24d429ab28
aleksandar.arsenovic <salle18@gmail.com>
2016-01-26 14:51:27 +0100

    csmp dialog draggable

--- app/components/csmp-dialog/csmp-dialog.controller.ts
+++ app/components/csmp-dialog/csmp-dialog.controller.ts
@@ -1,17 +1,28 @@
-import {Component} from "angular2/core";
+import {Component, OnInit, ElementRef} from "angular2/core";
 import {IJSONSimulation} from "../../csmp/Simulation";
 import {AppService} from "../../core/services/AppService";
 
 @Component({
 	selector: "csmp-dialog",
-	templateUrl: "components/csmp-dialog/csmp-dialog.template.html"
+	templateUrl: "components/csmp-dialog/csmp-dialog.template.html",
+	host: {
+		class: "mdl-card mdl-shadow--3dp"
+	}
 })
 export class CsmpDialog {
 
 	private simulations:IJSONSimulation[];
 
-	constructor(private appService:AppService) {
+	constructor(private appService:AppService, private elementRef:ElementRef) {
 		this.simulations = this.appService.simulations;
+	}
+
+	ngOnInit():void {
+		jQuery(this.elementRef.nativeElement).draggable();
+	}
+
+	removeSimulation(id:string):void {
+		this.appService.removeSimulation(id);
 	}
 
 }
