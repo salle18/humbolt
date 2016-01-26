0bf9cd1ddfffa3b0950550c3b7c01dc785c9efff
aleksandar.arsenovic <salle18@gmail.com>
2016-01-26 13:06:57 +0100

    open dialog dodat

--- app/components/csmp-menu/csmp-menu.controller.ts
+++ app/components/csmp-menu/csmp-menu.controller.ts
@@ -1,17 +1,18 @@
-import {Component, DoCheck} from "angular2/core";
+import {Component} from "angular2/core";
 import {RouterLink} from "angular2/router";
 import {CsmpUpgradeElement} from "../../directives/csmp-upgrade-element";
 import {SimulationService} from "../../core/services/SimulationService";
 import {AppService} from "../../core/services/AppService";
 import {IMetaJSONMethod, ISimulationConfig} from "../../csmp/Simulation";
 import {CsmpDialog} from "../csmp-dialog/csmp-dialog.controller";
+import {IJSONSimulation} from "../../csmp/Simulation";
 
 @Component({
 	selector: "csmp-menu",
 	templateUrl: "components/csmp-menu/csmp-menu.template.html",
 	directives: [CsmpUpgradeElement, CsmpDialog, RouterLink]
 })
-export class CsmpMenu implements DoCheck {
+export class CsmpMenu {
 
 	public simulationConfig:ISimulationConfig;
 	public methods:IMetaJSONMethod[] = [];
@@ -19,35 +20,32 @@ export class CsmpMenu implements DoCheck {
 
 	constructor(private appService:AppService, private simulationService:SimulationService) {
 		this.simulationConfig = simulationService.getSimulationConfig();
+		this.methods = this.appService.integrationMethods;
 	}
 
-	newSimulation() {
+	newSimulation():void {
 		this.appService.reset();
 	}
 
-	openSimulation() {
-		this.appService.open();
+	openDialog():void {
+		this.appService.listSimulations();
+		this.showDialog = true;
 	}
 
-	saveSimulation() {
+	saveSimulation():void {
 		this.appService.save();
 	}
 
-	runSimulation() {
+	runSimulation():void {
 		this.appService.run(this.simulationConfig);
 	}
 
-	rotateBlock(direction:string) {
+	rotateBlock(direction:string):void {
 		this.appService.rotateActiveBlock(direction);
 	}
 
-	removeBlock() {
+	removeBlock():void {
 		this.appService.removeActiveBlock();
 	}
 
-	ngDoCheck() {
-		if (!this.methods.length) {
-			this.methods = this.appService.getIntegrationMethods();
-		}
-	}
 }
