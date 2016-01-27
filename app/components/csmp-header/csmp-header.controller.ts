6c6fc8db5cd42425d5253196e56955bc8b4377f1
aleksandar.arsenovic <salle18@gmail.com>
2016-01-27 10:45:47 +0100

    dodat csmp-header

---
+++ app/components/csmp-header/csmp-header.controller.ts
@@ -0,0 +1,9 @@
+import {Component} from "angular2/core";
+
+@Component({
+	selector: "csmp-header",
+	templateUrl: "components/csmp-header/csmp-header.template.html"
+})
+export class CsmpHeader {
+
+}
