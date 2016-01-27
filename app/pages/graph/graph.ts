5410539cde09586775526a34d17436cf349f0cfe
aleksandar.arsenovic <salle18@gmail.com>
2016-01-27 10:44:40 +0100

    dodat csmp header

--- app/pages/graph/graph.ts
+++ app/pages/graph/graph.ts
@@ -1,11 +1,12 @@
 import { Component } from "angular2/core";
 import {RouterLink} from "angular2/router";
 import {CsmpGraph} from "../../components/csmp-graph/csmp-graph.controller";
+import {CsmpHeader} from "../../components/csmp-header/csmp-header.controller";
 
 @Component({
 	selector: "page-graph",
 	templateUrl: "pages/graph/graph.template.html",
-	directives: [CsmpGraph, RouterLink]
+	directives: [CsmpGraph, RouterLink, CsmpHeader]
 })
 export class Graph {
 
