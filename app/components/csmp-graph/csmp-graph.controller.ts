a313f5a8c47e7d01bf06164a3337c050d8e3dc56
aleksandar.arsenovic <salle18@gmail.com>
2016-01-27 10:01:03 +0100

    graph controller

--- app/components/csmp-graph/csmp-graph.controller.ts
+++ app/components/csmp-graph/csmp-graph.controller.ts
@@ -1,9 +1,21 @@
-import {Component} from "angular2/core";
+import {Component, ElementRef, OnInit} from "angular2/core";
+import * as Dygraph from "dygraphs";
+import {SimulationService} from "../../core/services/SimulationService";
+
+declare var Dygraph:any;
 
 @Component({
 	selector: "csmp-graph",
 	templateUrl: "components/csmp-graph/csmp-graph.template.html"
 })
-export class CsmpGraph {
+export class CsmpGraph implements OnInit {
+
+	constructor(private elementRef:ElementRef, private simulationService:SimulationService) {
+
+	}
+
+	ngOnInit():void {
+		let graph = new Dygraph(this.elementRef.nativeElement, this.simulationService.getSimulationResults());
+	}
 
 }
