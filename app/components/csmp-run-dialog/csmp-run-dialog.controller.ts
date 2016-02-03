9119ffd60958041416fe567d721bdd516ddcd50d
aleksandar.arsenovic <salle18@gmail.com>
2016-02-03 15:28:34 +0100

    typo

--- app/components/csmp-run-dialog/csmp-run-dialog.controller.ts
+++ app/components/csmp-run-dialog/csmp-run-dialog.controller.ts
@@ -1,5 +1,4 @@
 import {Component} from "angular2/core";
-import {ISimulationConfig} from "../../csmp/Simulation";
 import {AppService} from "../../core/services/AppService";
 import {ModalInstance} from "../../modules/modal/ModalInstance";
 import {SimulationService} from "../../core/services/SimulationService";
@@ -13,14 +12,11 @@ import {IJSONSimulation} from "../../csmp/Simulation";
 })
 export class CsmpRunDialog {
 
-	public simulationConfig:ISimulationConfig;
-	public methods:IMetaJSONMethod[] = [];
-
 	private config:ISimulationConfig;
+	private methods:IMetaJSONMethod[] = [];
 
 	constructor(private appService:AppService, private simulationService:SimulationService, private modalInstance:ModalInstance) {
 		this.config = this.simulationService.getSimulationConfig();
-		this.simulationConfig = simulationService.getSimulationConfig();
 		this.methods = this.appService.integrationMethods;
 	}
 
