f87a023a61de4cd4ee1c7a27fe9e1e0b76d74514
aleksandar.arsenovic <salle18@gmail.com>
2016-02-04 10:41:53 +0100

    dodata stranica configuration table

---
+++ app/pages/configuration-table/configuration-table.ts
@@ -0,0 +1,16 @@
+import {Component} from "angular2/core";
+import {RouterLink} from "angular2/router";
+import {CsmpConfigurationTable} from "../../components/csmp-configuration-table/csmp-configuration-table.controller";
+import {CsmpHeader} from "../../components/csmp-header/csmp-header.controller";
+
+@Component({
+	selector: "page-configuration-table",
+	templateUrl: "pages/results-table/results-table.template.html",
+	directives: [CsmpConfigurationTable, RouterLink, CsmpHeader]
+})
+export class ConfigurationTable {
+
+	constructor() {
+		console.log("ConfigurationTable component loaded");
+	}
+}
