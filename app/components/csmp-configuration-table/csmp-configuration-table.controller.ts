6fab4cc45d2dbcdae29f3b28db5ed08a79c96014
aleksandar.arsenovic <salle18@gmail.com>
2016-02-04 11:13:54 +0100

    ucitavanje blokova

--- app/components/csmp-configuration-table/csmp-configuration-table.controller.ts
+++ app/components/csmp-configuration-table/csmp-configuration-table.controller.ts
@@ -1,5 +1,6 @@
 import {Component} from "angular2/core";
 import {SimulationService} from "../../core/services/SimulationService";
+import {Block} from "../../csmp/Block";
 
 @Component({
 	selector: "csmp-configuration-table",
@@ -7,6 +8,9 @@ import {SimulationService} from "../../core/services/SimulationService";
 })
 export class CsmpConfigurationTable {
 
+	private blocks:Block[];
+
 	constructor(private simulationService:SimulationService) {
+		this.blocks = this.simulationService.getBlocks();
 	}
 }
