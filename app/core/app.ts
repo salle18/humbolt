b5145ae8649fbbf93099c8bdad704f004ec2daa2
aleksandar.arsenovic <salle18@gmail.com>
2016-02-04 10:54:05 +0100

    dodata stranica configurationTable

--- app/core/app.ts
+++ app/core/app.ts
@@ -15,6 +15,7 @@ import {Csmp} from "../pages/csmp/csmp";
 import {Gpss} from "../pages/gpss/gpss";
 import {Graph} from "../pages/graph/graph";
 import {ResultsTable} from "../pages/results-table/results-table";
+import {ConfigurationTable} from "../pages/configuration-table/configuration-table";
 
 // app services
 //import {appServicesInjectables} from "core/services/services";
@@ -30,7 +31,8 @@ import {ResultsTable} from "../pages/results-table/results-table";
 	{path: "/csmp", component: Csmp, as: "Csmp", data: undefined},
 	{path: "/gpss", component: Gpss, as: "Gpss", data: undefined},
 	{path: "/graph", component: Graph, as: "Graph", data: undefined},
-	{path: "/resultsTable", component: ResultsTable, as: "ResultsTable", data:undefined}
+	{path: "/resultsTable", component: ResultsTable, as: "ResultsTable", data: undefined},
+	{path: "/configurationTable", component: ConfigurationTable, as: "ConfigurationTable", data: undefined}
 ])
 export class App {
 	constructor() {
