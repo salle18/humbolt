16851abe416d24385ac66e210fb9e22353183ffc
aleksandar.arsenovic <salle18@gmail.com>
2016-02-04 15:09:12 +0100

    popravljena getFilteredSimulationResults

--- app/core/services/SimulationService.ts
+++ app/core/services/SimulationService.ts
@@ -56,9 +56,15 @@ export class SimulationService {
 
 	getFilteredSimulationResults():number[][] {
 		let results = this.getSimulationResults();
-		return results.filter((item, i) => {
-			return this.filters[i].value;
-		});
+		let filteredResults:number[][] = [];
+		console.log(results);
+		console.log(this.filters);
+		for (let i = 0; i < results.length; i++) {
+			filteredResults.push(results[i].filter((item, j) => {
+				return j===0 || this.filters[j - 1].value;
+			}));
+		}
+		return filteredResults;
 	}
 
 	initFilters():void {
