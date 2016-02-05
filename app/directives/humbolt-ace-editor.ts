b1ec4517d70035c53c4fbab0cfe836d042b722eb
aleksandar.arsenovic <salle18@gmail.com>
2016-02-05 15:43:51 +0100

    humbolt ace editor

---
+++ app/directives/humbolt-ace-editor.ts
@@ -0,0 +1,19 @@
+import {Directive, ElementRef, OnInit} from "angular2/core";
+var ace = require("ace");
+require("ace/theme-monokai");
+
+
+@Directive({
+	selector: "[humbolt-ace-editor]"
+})
+export class HumboltAceEditor implements OnInit {
+
+	constructor(private elementRef:ElementRef) {
+	}
+
+	ngOnInit():void {
+		let editor = ace.edit(this.elementRef.nativeElement);
+		editor.resize();
+		editor.setTheme("ace/theme/monokai");
+	}
+}
