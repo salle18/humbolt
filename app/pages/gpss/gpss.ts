00a5b3aa148e5bff599b0fd6666414f76ea30c34
aleksandar.arsenovic <salle18@gmail.com>
2016-02-05 15:43:19 +0100

    ace editor

--- app/pages/gpss/gpss.ts
+++ app/pages/gpss/gpss.ts
@@ -4,11 +4,12 @@ import {Component} from "angular2/core";
 import {GpssMenu} from "../../components/gpss-menu/gpss-menu.controller";
 import {CsmpHeader} from "../../components/csmp-header/csmp-header.controller";
 import {RouterLink} from "angular2/router";
+import {HumboltAceEditor} from "../../directives/humbolt-ace-editor";
 
 @Component({
 	selector: "page-gpss",
 	templateUrl: "pages/gpss/gpss.template.html",
-	directives: [CsmpHeader, GpssMenu, RouterLink]
+	directives: [CsmpHeader, GpssMenu, RouterLink, HumboltAceEditor]
 })
 export class Gpss {
 
