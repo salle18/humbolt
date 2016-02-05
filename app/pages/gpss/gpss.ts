f86644e73f07f816744753adc72f727780ebfabb
aleksandar.arsenovic <salle18@gmail.com>
2016-02-05 12:40:01 +0100

    gpss page

--- app/pages/gpss/gpss.ts
+++ app/pages/gpss/gpss.ts
@@ -1,12 +1,14 @@
 "use strict";
 
 import {Component} from "angular2/core";
+import {GpssMenu} from "../../components/gpss-menu/gpss-menu.controller";
+import {CsmpHeader} from "../../components/csmp-header/csmp-header.controller";
 import {RouterLink} from "angular2/router";
 
 @Component({
 	selector: "page-gpss",
 	templateUrl: "pages/gpss/gpss.template.html",
-	directives: [RouterLink]
+	directives: [CsmpHeader, GpssMenu, RouterLink]
 })
 export class Gpss {
 
