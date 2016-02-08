210b760e231b2a6404eda3428eaae7296fd3246a
aleksandar.arsenovic <salle18@gmail.com>
2016-02-08 13:59:59 +0100

    dodat typescript doc generator

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,7 @@
 "use strict";
 
 import gulp from "gulp";
+import typedoc from "gulp-typedoc";
 
 import modernWebDevBuild from "modern-web-dev-build";
 let options = {};
@@ -11,3 +12,15 @@ let options = {};
 options.minifyProductionHTML = false; // HTML minification is not supported by Angular 2 anymore: https://github.com/dsebastien/modernWebDevBuild/issues/67
 
 modernWebDevBuild.registerTasks(gulp, options);
+
+gulp.task("docs", function () {
+	return gulp
+		.src(["app/**/*.ts"])
+		.pipe(typedoc({
+			module: "commonjs",
+			target: "es5",
+			out: "docs/",
+			name: "Humbolt Client",
+			experimentalDecorators: true
+		}));
+});
