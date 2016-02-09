c4550ba94469f17186468e02c114eda817ea8b15
aleksandar.arsenovic <salle18@gmail.com>
2016-02-09 15:40:20 +0100

    dodati loader i hideLoader

--- app/core/services/MessageService.ts
+++ app/core/services/MessageService.ts
@@ -1,12 +1,29 @@
 import {Injectable} from "angular2/core";
 import {SnackbarService} from "../../modules/snackbar/SnackbarService";
+import {ModalService} from "../../modules/modal/ModalService";
+import {HumboltLoader} from "../../components/humbolt-loader/humbolt-loader.controller";
+import {ModalInstance} from "../../modules/modal/ModalInstance";
+
+export class Message {
+	constructor(public value:string) {
+	}
+}
 
 @Injectable()
 export class MessageService {
 
 	private timeout:number = 2000;
+	private modalInstance:Promise<ModalInstance>;
 
-	constructor(private snackbarService:SnackbarService) {
+	constructor(private snackbarService:SnackbarService, private modal:ModalService) {
+	}
+
+	loader():void {
+		this.modalInstance = this.modal.open(HumboltLoader, []);
+	}
+
+	hideLoader():void {
+		this.modalInstance.then(instance => instance.close());
 	}
 
 	success(message:string):void {
