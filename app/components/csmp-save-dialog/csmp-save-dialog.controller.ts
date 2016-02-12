bc1aedf729140b4376ef0a01feb9211519ccda18
aleksandar.arsenovic <salle18@gmail.com>
2016-02-12 10:41:47 +0100

    filename

--- app/components/csmp-save-dialog/csmp-save-dialog.controller.ts
+++ app/components/csmp-save-dialog/csmp-save-dialog.controller.ts
@@ -2,19 +2,17 @@ import {Component} from "angular2/core";
 import {CsmpAppService} from "../../core/services/CsmpAppService";
 import {ModalInstance} from "../../modules/modal/ModalInstance";
 import {SimulationService} from "../../core/services/SimulationService";
-import {ISimulationConfig} from "../../csmp/interfaces/ISimulationConfig";
 
 @Component({
 	selector: "csmp-save-dialog",
 	templateUrl: "components/csmp-save-dialog/csmp-save-dialog.template.html"
 })
 export class CsmpSaveDialog {
-
-	private config:ISimulationConfig;
 	private localFile:string;
+	private filename:string;
 
 	constructor(private appService:CsmpAppService, private simulationService:SimulationService, private modalInstance:ModalInstance) {
-		this.config = this.simulationService.getSimulationConfig();
+		this.filename = this.simulationService.getSimulationConfig().description + this.appService.extension.simulation;
 		this.localFile = this.appService.createLocalFile(this.simulationService.saveJSON());
 	}
 
