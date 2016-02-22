feb73eac5168bc9675841e10aafcc0f6608a0f24
aleksandar.arsenovic <salle18@gmail.com>
2016-02-22 15:12:46 +0100

    typings

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,4 +1,7 @@
 /// <reference path="browser\ambient\ace\ace.d.ts" />
+/// <reference path="browser\ambient\es6-shim\es6-shim.d.ts" />
+/// <reference path="browser\ambient\jasmine\jasmine.d.ts" />
 /// <reference path="browser\ambient\jquery.jsPlumb\jquery.jsPlumb.d.ts" />
 /// <reference path="browser\ambient\jquery\jquery.d.ts" />
 /// <reference path="browser\ambient\jqueryui\jqueryui.d.ts" />
+/// <reference path="browser\ambient\zone.js\zone.js.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,4 +1,7 @@
 /// <reference path="main\ambient\ace\ace.d.ts" />
+/// <reference path="main\ambient\es6-shim\es6-shim.d.ts" />
+/// <reference path="main\ambient\jasmine\jasmine.d.ts" />
 /// <reference path="main\ambient\jquery.jsPlumb\jquery.jsPlumb.d.ts" />
 /// <reference path="main\ambient\jquery\jquery.d.ts" />
 /// <reference path="main\ambient\jqueryui\jqueryui.d.ts" />
+/// <reference path="main\ambient\zone.js\zone.js.d.ts" />
